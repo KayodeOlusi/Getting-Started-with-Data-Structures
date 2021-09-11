4ee844d2d7001d3e31aefa4f82d727702efbdc13
KayodeOlusi <Kayodeolusi@gmail.com>
2021-09-11 18:13:39 +0100

    ...

--- Data Structures & Algorithm/functions/recursion.js
+++ Data Structures & Algorithm/functions/recursion.js
@@ -1,59 +1,95 @@
-//Practical ways to use recursion in javascript
+// //Practical ways to use recursion in javascript
 
-// //to create a factorial function
-function factorials(num) {
-  if (num < 2) {
-    //our base case
-    return 1;
-  } else {
-    return num * factorials(num - 1);
-    //the function repeats itself until the num argument gets to 1 value
+// // //to create a factorial function
+// function factorials(num) {
+//   if (num < 2) {
+//     //our base case
+//     return 1;
+//   } else {
+//     return num * factorials(num - 1);
+//     //the function repeats itself until the num argument gets to 1 value
+//   }
+// }
+
+// factorials(1); // 1
+// factorials(2); // 2
+// factorials(3); // 6
+// factorials(4); // 24
+// factorials(5); // 120
+
+// // to create a fibonacci series function
+// var fibonacci_series = function (n) {
+//   if (n === 1) {
+//     return [0, 1];
+//     //our base case
+//   } else {
+//     var fb = fibonacci_series(n - 1);
+//     //we store the returned values of our callback function into the variable fb
+//     fb.push(fb[fb.length - 1] + fb[fb.length - 2]);
+//     //we add the last value with the second to the last value and push into the array created from our base case  
+//     return fb;
+//     //fb becomes an array of the first n numbers of a fibonacci series
+//   }
+// };
+
+// console.log(fibonacci_series(1)); // [0,1] 
+// console.log(fibonacci_series(3)); // [0,1,1,2]
+// console.log(fibonacci_series(8)); // [0,1,1,2,3,5,8,13,21]
+
+// // to create a combination function
+// function combination(n, r) {
+//   if (n === r) {
+//     return 1;
+//     //our base case
+//   } else if (n < r) {
+//     console.log("Math Error");
+//     //if first input is less than second input
+//   } else {
+//     return (
+//       (n * factorials(n - 1)) /
+//       (r * factorials(r - 1) * ((n - r) * factorials(n - r - 1)))
+//     );
+//     //our combination function using recursion
+//   }
+// }
+
+// //To get the fibonacci of a number
+// const fibonacci = ( num ) => {
+//   if( num === 1 || num === 2 ) {
+//     return 1;
+//     //our base case to return 1 if num = 2 or 1
+//   } else if( num <= 0 ) {
+//     return 0;
+//     //our base case to return 0 if num is negative or equal to zero
+//   }
+
+//   return fibonacci( num - 1 ) + fibonacci( num - 2 );
+//   // the function calls itself until we reach our base case and returns a value 
+// }
+
+// console.log(fibonacci(1)) // 1
+// console.log(fibonacci(3)) // 2
+// console.log(fibonacci(6)) // 8
+// console.log(fibonacci(12)) // 144
+
+
+// console.log(factorials(5)); //120
+// console.log(factorials(3)); //6
+// console.log(combination(5, 3)); //10
+// console.log(combination(3, 4)); //Math Error
+
+const addSum = ( array ) => {
+  let sum = 0
+  for ( i = 0; i < array.length; i++ ) {
+    const current = array[i];
+    if ( Array.isArray(current) ) {
+      sum += addSum(current)
+    }else {
+      sum += current
+    }
   }
+  return sum
 }
 
-factorials(1); // 1
-factorials(2); // 2
-factorials(3); // 6
-factorials(4); // 24
-factorials(5); // 120
-
-// to create a fibonacci series function
-var fibonacci_series = function (n) {
-  if (n === 1) {
-    return [0, 1];
-    //our base case
-  } else {
-    var fb = fibonacci_series(n - 1);
-    //we store the returned values of our callback function into the variable fb
-    fb.push(fb[fb.length - 1] + fb[fb.length - 2]);
-    //we add the last value with the second to the last value and push into the array created from our base case  
-    return fb;
-    //fb becomes an array of the first n numbers of a fibonacci series
-  }
-};
-
-console.log(fibonacci_series(1)); // [0,1] 
-console.log(fibonacci_series(3)); // [0,1,1,2]
-console.log(fibonacci_series(8)); // [0,1,1,2,3,5,8,13,21]
-
-// to create a combination function
-function combination(n, r) {
-  if (n === r) {
-    return 1;
-    //our base case
-  } else if (n < r) {
-    console.log("Math Error");
-    //if first input is less than second input
-  } else {
-    return (
-      (n * factorials(n - 1)) /
-      (r * factorials(r - 1) * ((n - r) * factorials(n - r - 1)))
-    );
-    //our combination function using recursion
-  }
-}
-
-console.log(factorials(5)); //120
-console.log(factorials(3)); //6
-console.log(combination(5, 3)); //10
-console.log(combination(3, 4)); //Math Error
+console.log(addSum([1, [12, 3, 4], 2, [4, 5, 7], 3]))
+console.log(addSum([1, [12, 3, 4], 2, [4, 5, 7], 3]))
--- Data Structures & Algorithm/functions/sort.js
+++ Data Structures & Algorithm/functions/sort.js
@@ -1,52 +1,111 @@
-//How to use bubble sorting in javascript to sort an array
+// // How to use bubble sorting in javascript to sort an array
 
-//Define your array
-var num = [1, 3, 4, 2, 5, 7, 6, 10, 9, 8];
+// // Define your array
+// var num = [1, 3, 4, 2, 5, 7, 6, 10, 9, 8];
 
-const bubbleSort = (num) => {
-  //Create an empty array
-  var empty = [];
-  //Using do while loop
-  do {
-    //we set our base case(swapped) to false until the array is sorted
-    var swapped = false;
-    for (let i = 0; i < num.length; i++) {
-      //loop through the array
-      if (num[i] > num[i + 1]) {
-        //if the first value is greater than the next value
-        [num[i], num[i + 1]] = [num[i + 1], num[i]];
-        //swap the position the two values using destructuring
-        var swapped = true;
-        //set swapped to tue
-        console.log(num);
-      }
+// const bubbleSort = (num) => {
+//   //Create an empty array
+//   var empty = [];
+//   //Using do while loop
+//   do {
+//     //we set our base case(swapped) to false until the array is sorted
+//     var swapped = false;
+//     for (let i = 0; i < num.length; i++) {
+//       //loop through the array
+//       if (num[i] > num[i + 1]) {
+//         //if the first value is greater than the next value
+//         [num[i], num[i + 1]] = [num[i + 1], num[i]];
+//         //swap the position the two values using destructuring
+//         var swapped = true;
+//         //set swapped to tue
+//         console.log(num);
+//       }
+//     }
+//   } while (swapped); //As long as swapped is false it keeps sorting till its true
+//   empty.push(num);
+//   return empty;
+//   //push the sorted array to the empty array and return the empty array
+// };
+
+// bubbleSort(num);
+
+// //Using insertion sort to sort an array
+// var num = [4, 1, 2, 3, 7, 6, 5, 8, 10, 9];
+
+// const insertionSort = (num) => {
+//   var empty = [];
+
+//   for (let i = 1; i < num.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (num[i] < num[j]) {
+//         const spliced = num.splice(i, 1);
+//         num.splice(j, 0, spliced[0]);
+//         console.log(num);
+//       }
+//     }
+//   }
+
+//   empty.push(num);
+//   return empty;
+// };
+
+// insertionSort(num);
+
+const quickSort = ( nums ) => {
+    if (nums.length <= 2) {
+        return nums
     }
-  } while (swapped); //As long as swapped is false it keeps sorting till its true
-  empty.push(num);
-  return empty;
-  //push the sorted array to the empty array and return the empty array
-};
 
-bubbleSort(num);
+    const left = []
+    const right = []
+    const pivot = nums[ nums.length - 1 ]
 
-//Using insertion sort to sort an array
-var num = [4, 1, 2, 3, 7, 6, 5, 8, 10, 9];
-
-const insertionSort = (num) => {
-  var empty = [];
-
-  for (let i = 1; i < num.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (num[i] < num[j]) {
-        const spliced = num.splice(i, 1);
-        num.splice(j, 0, spliced[0]);
-        console.log(num);
-      }
+    for (let i = 0; i < nums.length - 1; i++) {
+        if ( nums[i] < pivot ) {
+            left.push( nums[i] )
+        } else {
+            right.push( nums[i] )
+        }
     }
-  }
 
-  empty.push(num);
-  return empty;
-};
+    const sortedLeft = quickSort(left)
+    const sortedRight = quickSort(right)
 
-insertionSort(num);
+    return sortedLeft.concat( pivot, sortedRight )
+}
+
+console.log(quickSort([10,2,4,3,1,6,5,8,7,9]))
+console.log(quickSort([10,2]))
+
+
+//Merge Sort
+// const mergeSort = ( nums ) => {
+//   if ( nums.length < 2 ) {
+//     return nums
+//   }
+
+//   const length = nums.length
+//   const middle = Math.floor( length / 2 )
+//   const left = nums.slice( 0, middle )
+//   const right = nums.slice( middle )
+//   const sortedLeft = mergeSort( left )
+//   const sortedRight = mergeSort( right )
+
+//   return merge( sortedLeft, sortedRight )
+// }
+
+// const merge = ( left, right ) => {
+//   const result = []
+
+//   while ( left.length && right.length ) {
+//     if ( left[0] <= right[0] ) {
+//         result.push(left.shift())
+//     } else {
+//       result.push(right.shift())
+//     }
+//   }
+
+//   result.concat( left, right )
+// }
+
+// console.log(mergeSort([10,2,4,3,1,6,5,8,7,9]))
