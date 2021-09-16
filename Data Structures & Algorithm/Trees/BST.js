2273d6700197ae0447bda5f0b67aad7760826dca
KayodeOlusi <Kayodeolusi@gmail.com>
2021-09-16 04:18:07 +0100

    added codes for trees and branches

---
+++ Data Structures & Algorithm/Trees/BST.js
@@ -0,0 +1,50 @@
+// Binary Search Tree works on a sorted list or array
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    add( value ) {
+        if ( this.root === null ) {
+            this.root = new Node(value); 
+        } else {
+            let current = this.root;
+            while (true) {
+                if (current.value > value) {
+                    // go left
+
+                    if ( current.left ) {
+                        current = current.left;
+                    } else {
+                        current.left = new Node(value);
+                        break;
+                    }
+                }else {
+                    // go right
+
+                    if ( current.right ) {
+                        current = current.right;
+                    } else {
+                        current.right = new Node(value);
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    toObject () {
+        return this.root;
+    }
+}
+
+
+class Node {
+    constructor ( value ) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ Data Structures & Algorithm/Trees/FTT.js
@@ -0,0 +1,25 @@
+//  Depth First Tree Traversals
+
+const preOrderTraverse = ( node, array ) => {
+    if (!node) return array;
+    array.push(node.value);
+    array = preOrderTraverse(node.left, array);
+    array = preOrderTraverse(node.right, array);
+    return array;
+}
+
+const innerOrderTraverse = ( node, array ) => {
+    if (!node) return array;
+    array = preOrderTraverse(node.left, array);
+    array.push(node.value);
+    array = preOrderTraverse(node.right, array);
+    return array;
+}
+
+const postOrderTraverse = ( node, array ) => {
+    if (!node) return array;
+    array = preOrderTraverse(node.left, array);
+    array = preOrderTraverse(node.right, array);
+    array.push(node.value);
+    return array;
+}
