5ea43855b58c7e53a3857252fc89f210b1912d39
KayodeOusi <kayodeolusi@gmail.com>
2022-04-12 16:54:38 +0100

    changes

--- Data Structures & Algorithm/functions/sort.js
+++ Data Structures & Algorithm/functions/sort.js
@@ -3,110 +3,104 @@
 // Define your array
 var num = [1, 3, 4, 2, 5, 7, 6, 10, 9, 8];
 
-
 const bubbleSort = (num) => {
-  //Create an empty array
-  var empty = [];
-  //Using do while loop
-  do {
-    //we set our base case(swapped) to false until the array is sorted
-    var swapped = false;
-    for (let i = 0; i < num.length; i++) {
-      //loop through the array
-      if (num[i] > num[i + 1]) {
-        //if the first value is greater than the next value
-        [num[i], num[i + 1]] = [num[i + 1], num[i]];
-        //swap the position the two values using destructuring
-        var swapped = true;
-        //set swapped to tue
-        console.log(num);
-      }
-    }
-  } while (swapped); //As long as swapped is false it keeps sorting till its true
-  empty.push(num);
-  return empty;
-  //push the sorted array to the empty array and return the empty array
+	//Create an empty array
+	var empty = [];
+	//Using do while loop
+	do {
+		//we set our base case(swapped) to false until the array is sorted
+		var swapped = false;
+		for (let i = 0; i < num.length; i++) {
+			//loop through the array
+			if (num[i] > num[i + 1]) {
+				//if the first value is greater than the next value
+				[num[i], num[i + 1]] = [num[i + 1], num[i]];
+				//swap the position the two values using destructuring
+				var swapped = true;
+				//set swapped to tue
+				console.log(num);
+			}
+		}
+	} while (swapped); //As long as swapped is false it keeps sorting till its true
+	empty.push(num);
+	return empty;
+	//push the sorted array to the empty array and return the empty array
 };
-
 
 //Using insertion sort to sort an array
 const insertionSort = (num) => {
-  var empty = [];
+	var empty = [];
 
-  for (let i = 1; i < num.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (num[i] < num[j]) {
-        const spliced = num.splice(i, 1);
-        num.splice(j, 0, spliced[0]);
-        console.log(num);
-      }
-    }
-  }
+	for (let i = 1; i < num.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (num[i] < num[j]) {
+				const spliced = num.splice(i, 1);
+				num.splice(j, 0, spliced[0]);
+				console.log(num);
+			}
+		}
+	}
 
-  empty.push(num);
-  return empty;
+	empty.push(num);
+	return empty;
 };
 
-
 // Using Quick Sort
-const quickSort = ( nums ) => {
-    if (nums.length <= 2) {
-        return nums
-    }
+const quickSort = (nums) => {
+	if (nums.length <= 2) {
+		return nums;
+	}
 
-    const left = []
-    const right = []
-    const pivot = nums[ nums.length - 1 ]
+	const left = [];
+	const right = [];
+	const pivot = nums[nums.length - 1];
 
-    for (let i = 0; i < nums.length - 1; i++) {
-        if ( nums[i] < pivot ) {
-            left.push( nums[i] )
-        } else {
-            right.push( nums[i] )
-        }
-    }
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] < pivot) {
+			left.push(nums[i]);
+		} else {
+			right.push(nums[i]);
+		}
+	}
 
-    const sortedLeft = quickSort(left)
-    const sortedRight = quickSort(right)
+	const sortedLeft = quickSort(left);
+	const sortedRight = quickSort(right);
 
-    return sortedLeft.concat( pivot, sortedRight )
-}
+	return sortedLeft.concat(pivot, sortedRight);
+};
 
 // Merge Sort
-const mergeSort = ( nums ) => {
-  if ( nums.length < 2 ) {
-    return nums
-  }
+const mergeSort = (nums) => {
+	if (nums.length < 2) {
+		return nums;
+	}
 
-  const length = nums.length
-  const middle = Math.floor( length / 2 )
-  const left = nums.slice( 0, middle )
-  const right = nums.slice( middle )
-  const sortedLeft = mergeSort( left )
-  const sortedRight = mergeSort( right )
+	const length = nums.length;
+	const middle = Math.floor(length / 2);
+	const left = nums.slice(0, middle);
+	const right = nums.slice(middle);
+	const sortedLeft = mergeSort(left);
+	const sortedRight = mergeSort(right);
 
-  return merge( sortedLeft, sortedRight )
-}
+	return merge(sortedLeft, sortedRight);
+};
 
-const merge = ( left, right ) => {
-  const result = []
+const merge = (left, right) => {
+	const result = [];
 
-  while ( left.length && right.length ) {
-    if ( left[0] <= right[0] ) {
-        result.push(left.shift())
-    } else {
-      result.push(right.shift())
-    }
-  }
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
 
-  result.concat( left, right )
-}
-
-
+	result.concat(left, right);
+};
 
 bubbleSort(num);
 insertionSort(num);
-console.log(quickSort([10,2,4,3,1,6,5,8,7,9]))
-console.log(quickSort([10,2]))
-console.log(mergeSort([10,2,4,3,1,6,5,8,7,9]))
-
+console.log(quickSort([10, 2, 4, 3, 1, 6, 5, 8, 7, 9]));
+console.log(quickSort([10, 2]));
+console.log(mergeSort([10, 2, 4, 3, 1, 6, 5, 8, 7, 9]));
