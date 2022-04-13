fba258b1b2cc9dfa527c0cf55a6df6c80ea53fe9
KayodeOusi <kayodeolusi@gmail.com>
2022-04-13 15:36:57 +0100

    Radix sort

--- Data Structures & Algorithm/functions/sort.js
+++ Data Structures & Algorithm/functions/sort.js
@@ -114,6 +114,45 @@ const merge = (left, right) => {
 	result.concat(left, right);
 };
 
+// Radix Sort
+function getDigit(number, place, longestNumber) {
+	const string = number.toString();
+	const size = string.length;
+
+	const mod = longestNumber - size;
+	return string[place - mod] || 0;
+}
+
+function findLongestNumber(array) {
+	let longest = 0;
+	for (let i = 0; i < array.length; i++) {
+		const currentLength = array[i].toString().length;
+		longest = currentLength > longest ? currentLength : longest;
+	}
+	return longest;
+}
+
+function radixSort(array) {
+	const longestNumber = findLongestNumber(array);
+
+	const buckets = new Array(10).fill().map(() => []); // make an array of 10 arrays
+
+	for (let i = longestNumber - 1; i >= 0; i--) {
+		while (array.length) {
+			const current = array.shift();
+			buckets[getDigit(current, i, longestNumber)].push(current);
+		}
+
+		for (let j = 0; j < 10; j++) {
+			while (buckets[j].length) {
+				array.push(buckets[j].shift());
+			}
+		}
+	}
+
+	return array;
+}
+
 // bubbleSort(num);
 // insertionSort(num);
 console.log(quickSort([10, 2, 4, 3, 1, 6, 5, 8, 7, 9]));
